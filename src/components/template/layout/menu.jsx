e71b232aa9039f9930ca2207634285d8eb944a45
henrique.souza <henrique.souza@iesb.br>
2020-02-03 09:32:26 -0300

    alteracao de icones do menu

--- src/components/template/layout/menu.jsx
+++ src/components/template/layout/menu.jsx
@@ -9,10 +9,10 @@ function Menu(){
         <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <MenuLink description={`Dashboard`} path={`/`} icon={`fas fa-tachometer-alt`}  active={``} />
-                <MenuTreeView description={`Atividades Compl.`} path={`#`} icon={`fas fa-check`} >
-                    <MenuLink description={`Alunos`} path={`/atividades-complementares/alunos`} icon={`fas fa-check`} active={``} />
-                    <MenuLink description={`Atividades`} path={`/atividades-complementares/atividades`} icon={`fas fa-check`} active={``}/>
-                    <MenuLink description={`Categorias`} path={`/atividades-complementares/categorias`} icon={`fas fa-check`} active={``}/>
+                <MenuTreeView description={`Atividades Compl.`} path={`#`} icon={`fas fa-tasks`} >
+                    <MenuLink description={`Alunos`} path={`/atividades-complementares/alunos`} icon={`fa fa-users`} active={``} />
+                    <MenuLink description={`Atividades`} path={`/atividades-complementares/atividades`} icon={`fa fa-list-alt`} active={``}/>
+                    <MenuLink description={`Categorias`} path={`/atividades-complementares/categorias`} icon={`fas fa-th-large`} active={``}/>
                 </MenuTreeView>
             </ul>
         </nav>
