ff19fc7abcb5cc0f979fb6260dd40987d993fc5b
henrique.souza <henrique.souza@iesb.br>
2020-03-31 10:20:39 -0300

    mudança de apontamento de ambiente

--- src/config/const.js
+++ src/config/const.js
@@ -1,16 +1,14 @@
 
 export const BASE_API = (
 
-    process.env.NODE_ENV === 'production' ? 
+    // process.env.NODE_ENV === 'production' ? 
         // 'http://spcomapi.cnec.br/api'
-        'http://desenv.iesb.br/henrique.souza/sistemasAcademicos/web-service/'
-    :
-    process.env.NODE_ENV === 'homologation' ?
-
-        console.log('homologation')
+    // :
+    // process.env.NODE_ENV === 'homologation' ?
+        // console.log('homologation')
         // 'http://hmlapidescontos.cnec.br/api'
-    :
+    // :
         // console.log('desenv')
-        'http://desenv.iesb.br/henrique.souza/sistemasAcademicos/web-service/'
+        'http://apps.iesb.br/sistemasAcademicos/web-service/'
 
 );
